b724078ffa4543e31370b31298b41a36a709992a
Andrés Martínez <autummata@gmail.com>
2024-04-14 00:16:39 -0700

    Add: Web Socket to Port 3005

--- src/func.rs
+++ src/func.rs
@@ -2,7 +2,5 @@ use rand::Rng;
 
 pub(crate) fn get_random_number() -> i32 {
     let mut rng = rand::thread_rng();
-
-    let n1: i32 = rng.gen_range(0..10);
-    return n1;
+    return rng.gen_range(0..10);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+use axum::{
+    routing::get,
+    Router,
+};
 mod func;
 
-fn get_grade(grade: i32) -> &'static str {
-    match grade {
-        90..=100=>"A",
-        80..=89=>"B",
-        70..=79=>"C",
-        60..=69=>"D",
-        _=>"F"
-    }
-}
+#[tokio::main]
+async fn main() {
+    const PORT: i32 = 3005;
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, world!\n"}))
+        .route("/random", get(|| async { func::get_random_number().to_string() }));
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", PORT)).await.unwrap();
 
-fn main() {
-    println!("{}", func::get_random_number());
+    println!("{}", format!("It is currently working on the port {}!", PORT));
+    axum::serve(listener, app).await.unwrap();
 }
 
