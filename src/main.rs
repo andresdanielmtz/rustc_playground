0328148473706b531267bf571563721cbb54518f
Andrés Martínez <autummata@gmail.com>
2024-04-14 11:42:56 -0700

    Fix: Typos, refactored lines for better understanding

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use axum::{
-    routing::get,
     Router,
+    routing::get,
 };
 mod func;
 
 #[tokio::main]
 async fn main() {
-    const PORT: i32 = 3005;
+    const PORT: i16 = 3005; // 3000 is busy.
     let app = Router::new()
         .route("/", get(|| async { "Hello, world!\n"}))
         .route("/random", get(|| async { func::get_random_number().to_string() }));
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", PORT)).await.unwrap();
 
-    println!("{}", format!("It is currently working on the port {}!", PORT));
+    println!("Listening on port {}!", PORT);
     axum::serve(listener, app).await.unwrap();
 }
 
